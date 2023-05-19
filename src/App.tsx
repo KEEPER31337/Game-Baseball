4f70393cb9cc8ccf04b9d338c2fff45721f33140
publdaze <publdaze@naver.com>
2023-05-19 15:40:14 +0900

    feat : 게임 화면 중앙 정렬
    #8

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <div className="App">baseball game</div>;
+  return <div className="grid h-screen w-screen place-items-center">baseball game</div>;
 };
 
 export default App;
