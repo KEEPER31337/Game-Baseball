8a93972ac8dc47e105e4416063882d9493ea7866
jasper200207 <jasper12394@gmail.com>
2023-06-04 16:23:35 +0900

    feat : props 설정
    - #6

--- src/components/TurnInfoBoard.tsx
+++ src/components/TurnInfoBoard.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-const TurnInfoBoard = () => {
+interface ResultDto {
+  ball: number;
+  strike: number;
+}
+
+interface TurnInfoBoardProps {
+  results: Array<ResultDto | null>;
+  round: number;
+}
+
+const TurnInfoBoard = ({ results, round }: TurnInfoBoardProps) => {
   return <div>모든 회차 정보 섹션 입니다.</div>;
 };
 
