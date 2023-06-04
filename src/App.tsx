3f7f96f18d2a4010918d7aeeb12f2db050cc53ef
KimHaeJoong1 <hjkim2124@gmail.com>
2023-06-04 15:59:06 +0900

    feat : 게임 시작 화면 구성
    - 게임 시작 화면 구성입니다
    - 포인트 입력 없거나 조건에 맞지 않으면 스타트 비활성화해두었습니다
    - #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GameStart from './screens/GameStart';
 import GamePlay from './screens/GamePlay';
 
 const App = () => {
-  const playMode = false;
-  // TODO const [playMode, setPlayMode] = useState(false);
+  const [playMode, setPlayMode] = useState(false);
+
+  const onStart = () => {
+    setPlayMode(true);
+  };
 
   return (
     <div className="grid h-screen w-screen place-items-center bg-mainBlack">
-      {playMode ? <GamePlay /> : <GameStart />}
+      {playMode ? <GamePlay /> : <GameStart onStart={onStart} />}
     </div>
   );
 };
--- src/screens/GameStart.tsx
+++ src/screens/GameStart.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const GameStart = () => {
-  return <div>시작화면 입니다.</div>;
+const GameStart = ({ onStart }: { onStart: any }) => {
+  const [point, setPoint] = useState<string>('');
+
+  return (
+    <div className="flex h-full w-full flex-col place-content-center place-items-center">
+      <div className="mb-10 text-[40px] font-bold text-pointBlue">BASEBALL GAME</div>
+      <div className="mb-10 text-2xl">batting your point...</div>
+      <input
+        value={point}
+        onChange={(e) => {
+          setPoint(e.target.value.replace(/[^0-9]/g, ''));
+        }}
+        className="mb-20 w-[400px] border-[1px] border-pointBlue bg-transparent text-center text-[40px] focus:outline-none"
+        type="text"
+        placeholder="10 ~ 1000"
+      />
+      <button
+        disabled={parseInt(point, 10) > 1000 || parseInt(point, 10) < 10 || point === ''}
+        className="text-2xl enabled:hover:text-pointBlue"
+        onClick={onStart}
+        type="button"
+      >
+        START
+      </button>
+    </div>
+  );
 };
 
 export default GameStart;
