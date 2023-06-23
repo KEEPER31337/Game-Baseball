809daecca863acfbd04cc104ff9e7a03ac7bf1d0
신예준 <wns1826@gmail.com>
2023-06-23 15:15:36 +0900

    feat : 숫자 입력 컴포넌트 구현
    #7

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import OTPInput from 'otp-input-react';
 
 const NumberInput = () => {
-  return <div>숫자 입력 섹션</div>;
+  const [number, setNumber] = useState('');
+
+  return (
+    <OTPInput
+      value={number}
+      onChange={setNumber}
+      OTPLength={4}
+      otpType="number"
+      inputClassName="border border-dashed border-pointBlue bg-transparent text-[32px] focus:outline-none"
+      inputStyles={{
+        width: '50px',
+        height: '60px',
+        'margin-right': '5px',
+      }}
+      disabled={false}
+      autoFocus
+    />
+  );
 };
 
 export default NumberInput;
