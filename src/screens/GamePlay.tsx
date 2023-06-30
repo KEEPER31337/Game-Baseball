9761f0900dfc5496b004dacc549dc65d8cc9fd6a
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2023-06-30 21:57:22 +0900

    refactor : GamePlay.tsx 변경사항 추가
    - #6

--- src/screens/GamePlay.tsx
+++ src/screens/GamePlay.tsx
@@ -10,7 +10,10 @@ const GamePlay = () => {
     <div>
       <PointInfo />
       <CountdownBar />
-      <TurnInfoBoard />
+      <TurnInfoBoard
+        results={[{ ball: 1, strike: 2, guessNumber: '1234' }, null, { ball: 2, strike: 1, guessNumber: '2345' }]}
+        round={9}
+      />
       <div className="flex items-center space-x-4">
         <NumberInput />
         <CiBaseball size={50} className="cursor-pointer fill-pointBlue hover:rounded-full hover:bg-pointBlue/20" />
