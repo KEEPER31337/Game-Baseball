39ca030fb14189e39ca94335c688c357bf083ed3
jasper200207 <jasper12394@gmail.com>
2023-07-01 21:57:22 +0900

    feat : baseURL 설정
    - baseURL을 env파일의 정보로 수정
    - gitignore 파일에 env파일과 api 테스트용 파일 추가
    - #20

--- src/api/baseballApi.ts
+++ src/api/baseballApi.ts
@@ -10,25 +10,25 @@ const quearyKeys = {
 };
 
 const useGetIsAlreadyPlayed = () => {
-  const fetcher = () => axios.get('/api/baseball/is_already_played').then(({ data }) => data);
+  const fetcher = () => axios.get('/baseball/is_already_played').then(({ data }) => data);
 
   return useQuery<boolean>(quearyKeys.is_already_played, fetcher);
 };
 
 const useGameStart = ({ bettingPoint }: { bettingPoint: number }) => {
-  const fetcher = () => axios.post('/api/baseball/start', { bettingPoint }).then(({ data }) => data);
+  const fetcher = () => axios.post('/baseball/start', { bettingPoint }).then(({ data }) => data);
 
   return useMutation(fetcher);
 };
 
 const useGetGuess = ({ guessNumber }: { guessNumber: GuessNumber }) => {
-  const fetcher = () => axios.post('/api/baseball/guess', { guessNumber }).then(({ data }) => data);
+  const fetcher = () => axios.post('/baseball/guess', { guessNumber }).then(({ data }) => data);
 
   return useMutation(fetcher);
 };
 
 const useGetResult = () => {
-  const fetcher = () => axios.get('/api/baseball/result').then(({ data }) => data);
+  const fetcher = () => axios.get('/baseball/result').then(({ data }) => data);
 
   return useQuery<GameResultInfo>(quearyKeys.result, fetcher);
 };
