2b8d261586a247d751fec327cb74eeb515cbcfac
jasper200207 <jasper12394@gmail.com>
2023-07-16 16:08:05 +0900

    refactor : API 함수 이름 변경
    - 뒤에 Query 또는 Mutation 추가
    - useGetGuess에서 Get 제거 (GET 메서드가 아님)
    - #20

--- src/api/baseballApi.ts
+++ src/api/baseballApi.ts
@@ -8,34 +8,34 @@ const quearyKeys = {
   result: ['result'] as const,
 };
 
-const useGetGameInfo = () => {
+const useGetGameInfoQuery = () => {
   const fetcher = () => axios.get('/game/baseball/game-info').then(({ data }) => data);
 
   return useQuery<GameInfo>(quearyKeys.game_info, fetcher);
 };
 
-const useGetIsAlreadyPlayed = () => {
+const useGetIsAlreadyPlayedQuery = () => {
   const fetcher = () => axios.get('/game/baseball/is_already_played').then(({ data }) => data);
 
   return useQuery<boolean>(quearyKeys.is_already_played, fetcher);
 };
 
-const useGameStart = ({ bettingPoint }: { bettingPoint: number }) => {
+const useGameStartMutation = ({ bettingPoint }: { bettingPoint: number }) => {
   const fetcher = () => axios.post('/game/baseball/start', { bettingPoint }).then(({ data }) => data);
 
   return useMutation(fetcher);
 };
 
-const useGetGuess = ({ guessNumber }: { guessNumber: GuessNumber }) => {
+const useGuessMutation = ({ guessNumber }: { guessNumber: GuessNumber }) => {
   const fetcher = () => axios.post('/game/baseball/guess', { guessNumber }).then(({ data }) => data);
 
   return useMutation(fetcher);
 };
 
-const useGetResult = () => {
+const useGetResultQuery = () => {
   const fetcher = () => axios.get('/game/baseball/result').then(({ data }) => data);
 
   return useQuery<GameResultInfo>(quearyKeys.result, fetcher);
 };
 
-export { useGetGameInfo, useGetIsAlreadyPlayed, useGameStart, useGetGuess, useGetResult };
+export { useGetGameInfoQuery, useGetIsAlreadyPlayedQuery, useGameStartMutation, useGuessMutation, useGetResultQuery };
