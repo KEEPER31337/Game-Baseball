756e707d7698d9351f64df5fad9bf06fade453c7
pipisebastian <yrt7998@pusan.ac.kr>
2023-07-16 16:07:43 +0900

    refactor : main, sub text state 2개 1개로 통합
    #2

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -26,15 +26,17 @@ const InfoModal = ({ onClose, infoType, results }: InfoModalProps) => {
       sub: null,
     },
   };
-  const [mainInfoText, setMainInfoText] = useState<string>('');
-  const [subInfoText, setSubInfoText] = useState<string>('');
+
+  const [infoText, setInfoText] = useState({ main: '', sub: '' });
 
   const [count, setCount] = useState(3);
   const interval = useRef<NodeJS.Timeout>();
 
   useEffect(() => {
-    setMainInfoText(msg[infoType]?.main || String(results?.guessNumber));
-    setSubInfoText(msg[infoType]?.sub || `STRIKE ${String(results?.strike)} BALL ${String(results?.ball)}`);
+    setInfoText({
+      main: msg[infoType]?.main || String(results?.guessNumber),
+      sub: msg[infoType]?.sub || `STRIKE ${String(results?.strike)} BALL ${String(results?.ball)}`,
+    });
 
     interval.current = setInterval(() => {
       setCount((cnt) => cnt - 1);
@@ -53,8 +55,8 @@ const InfoModal = ({ onClose, infoType, results }: InfoModalProps) => {
   return (
     <div className="absolute left-0 top-0 z-10 flex h-screen w-screen items-center justify-center bg-black/50">
       <div className="flex h-[260px] w-[490px] flex-col justify-center space-y-4 border bg-black text-center text-4xl drop-shadow-[0px_0px_5px_rgba(76,238,249,0.3)]">
-        <p>{mainInfoText}</p>
-        <p className="drop-shadow-[0px_0px_1px_rgba(76,238,249,1)]">{subInfoText}</p>
+        <p>{infoText.main}</p>
+        <p className="drop-shadow-[0px_0px_1px_rgba(76,238,249,1)]">{infoText.sub}</p>
       </div>
     </div>
   );
