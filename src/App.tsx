401544feeac49f17e1f6be04583aeaf8ded47eba
pipisebastian <yrt7998@pusan.ac.kr>
2023-07-29 16:57:48 +0900

    feat : GameEnd컴포넌트 제거 및 GameStart내부에서 플레이 여부 api 호출

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import GameStart from './screens/GameStart';
 import GamePlay from './screens/GamePlay';
-import GameEnd from './screens/GameEnd';
-import { useGameStartMutation, useGetIsAlreadyPlayedQuery } from './api/baseballApi';
+import { useGameStartMutation } from './api/baseballApi';
 
 const App = () => {
   const [bettingPoint, setBettingPoint] = useState<string>('');
   const [playMode, setPlayMode] = useState(false);
-  const { data: isPlayed } = useGetIsAlreadyPlayedQuery();
   const { mutate: gameStart } = useGameStartMutation();
 
   const handleStart = () => {
@@ -17,16 +15,10 @@ const App = () => {
 
   return (
     <div className="grid h-screen w-screen place-items-center bg-mainBlack">
-      {isPlayed ? (
-        <GameEnd />
+      {playMode ? (
+        <GamePlay />
       ) : (
-        <div>
-          {playMode ? (
-            <GamePlay />
-          ) : (
-            <GameStart onStart={handleStart} bettingPoint={bettingPoint} setBettingPoint={setBettingPoint} />
-          )}
-        </div>
+        <GameStart onStart={handleStart} bettingPoint={bettingPoint} setBettingPoint={setBettingPoint} />
       )}
     </div>
   );
--- src/screens/GameStart.tsx
+++ src/screens/GameStart.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useGetIsAlreadyPlayedQuery } from '../api/baseballApi';
 
 const MIN_BETTING_POINT = 10;
 const MAX_BETTING_POINT = 1000;
@@ -10,31 +11,40 @@ interface GameStartProps {
 }
 
 const GameStart = ({ onStart, bettingPoint, setBettingPoint }: GameStartProps) => {
+  const { data: isPlayed } = useGetIsAlreadyPlayedQuery();
+
   return (
     <div className="flex h-full w-full flex-col place-content-center place-items-center">
       <div className="mb-10 text-[40px] font-bold text-pointBlue">BASEBALL GAME</div>
-      <div className="mb-10 text-2xl">betting your point...</div>
-      <input
-        value={bettingPoint}
-        onChange={(e) => {
-          setBettingPoint(e.target.value.replace(/[^0-9]/g, ''));
-        }}
-        className="mb-20 w-[400px] border-[1px] border-pointBlue bg-transparent text-center text-[40px] focus:outline-none"
-        type="text"
-        placeholder={`${MIN_BETTING_POINT} ~ ${MAX_BETTING_POINT}`}
-      />
-      <button
-        disabled={
-          parseInt(bettingPoint, 10) > MAX_BETTING_POINT ||
-          parseInt(bettingPoint, 10) < MIN_BETTING_POINT ||
-          bettingPoint === ''
-        }
-        className="text-2xl enabled:hover:text-pointBlue disabled:text-gray-500"
-        onClick={onStart}
-        type="button"
-      >
-        START
-      </button>
+
+      {isPlayed ? (
+        <div className="mb-10 text-2xl">You$ve already played the game</div>
+      ) : (
+        <div className="flex flex-col place-content-center place-items-center">
+          <div className="mb-10 text-2xl">Please enter your betting point!</div>{' '}
+          <input
+            value={bettingPoint}
+            onChange={(e) => {
+              setBettingPoint(e.target.value.replace(/[^0-9]/g, ''));
+            }}
+            className="mb-20 w-[400px] border-[1px] border-pointBlue bg-transparent text-center text-[40px] focus:outline-none"
+            type="text"
+            placeholder={`${MIN_BETTING_POINT} ~ ${MAX_BETTING_POINT}`}
+          />
+          <button
+            disabled={
+              parseInt(bettingPoint, 10) > MAX_BETTING_POINT ||
+              parseInt(bettingPoint, 10) < MIN_BETTING_POINT ||
+              bettingPoint === ''
+            }
+            className="text-2xl enabled:hover:text-pointBlue disabled:text-gray-500"
+            onClick={onStart}
+            type="button"
+          >
+            START
+          </button>
+        </div>
+      )}
     </div>
   );
 };
