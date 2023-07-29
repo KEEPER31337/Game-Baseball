1565cdcbec8b5790c2a0aca2b67c264a1e8daae8
신예준 <wns1826@gmail.com>
2023-07-29 15:04:58 +0900

    refactor: state 이름 및 위치 수정

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AuthCode from 'react-auth-code-input';
 
-const NumberInput = () => {
-  const [number, setNumber] = useState('');
-  const handleOnChange = (res: string) => {
-    setNumber(res);
-  };
+interface NumberInputProps {
+  onChange: (res: string) => void;
+}
 
+const NumberInput = ({ onChange }: NumberInputProps) => {
   return (
     <AuthCode
       allowedCharacters="numeric"
-      onChange={handleOnChange}
+      onChange={onChange}
       length={4}
       inputClassName="w-[50px] h-[60px] border border-dashed border-pointBlue bg-transparent text-[32px] text-center mr-3 focus:outline-none"
       autoFocus
