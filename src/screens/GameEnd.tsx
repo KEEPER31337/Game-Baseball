c051e7d009e173344e1e33b57248076c9dbba7ce
pipisebastian <yrt7998@pusan.ac.kr>
2023-07-29 16:30:30 +0900

    feat : 이미 게임을 한 경우 나오는 GameEnd 컴포넌트 생성

---
+++ src/screens/GameEnd.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GameEnd = () => {
+  return (
+    <div className="flex h-full w-full flex-col place-content-center place-items-center">
+      <div className="mb-10 text-[40px] font-bold text-pointBlue">BASEBALL GAME</div>
+      <div className="mb-10 text-2xl">You&apos;ve already played the game!</div>
+    </div>
+  );
+};
+
+export default GameEnd;
