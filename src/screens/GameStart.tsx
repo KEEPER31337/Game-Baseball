31315cd82deb06900b44dd5214e9038d740c5f86
publdaze <publdaze@naver.com>
2023-08-19 11:26:08 +0900

    feat : 변경된 api 양식 적용 및 status에 따른 화면 제어
    #54

--- src/screens/GameStart.tsx
+++ src/screens/GameStart.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { useGetGameInfoQuery } from '../api/baseballApi';
+import { GameStatus } from '../api/dto';
 
 interface GameStartProps {
   onStart: () => void;
-  isPlayed: boolean;
+  status: GameStatus;
   bettingPoint: string;
   setBettingPoint: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const GameStart = ({ onStart, isPlayed, bettingPoint, setBettingPoint }: GameStartProps) => {
+const GameStart = ({ onStart, status, bettingPoint, setBettingPoint }: GameStartProps) => {
   const { data: gameInfo, isLoading: gameInfoLoading } = useGetGameInfoQuery();
-  const gamePlayedMessage = isPlayed ? "You've already played the game!" : 'betting your point...';
+  const gamePlayedMessage = {
+    NOT_START: 'betting your point...',
+    END: "You've already played the game!",
+    PLAYING: null,
+  };
 
   if (gameInfoLoading || !gameInfo) return null;
   return (
     <div className="flex h-full w-full flex-col place-content-center place-items-center">
       <div className="mb-10 text-[40px] font-bold text-pointBlue">BASEBALL GAME</div>
-      <div className="mb-10 text-2xl">{gamePlayedMessage}</div>
-      {!isPlayed && (
+      <div className="mb-10 text-2xl">{gamePlayedMessage[status]}</div>
+      {status === 'NOT_START' && (
         <>
           <input
             value={bettingPoint}
